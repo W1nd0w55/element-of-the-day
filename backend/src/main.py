3f0127772c75ca4bbf85dc69cc15403116a3845e
W1nd0w55 <iliader@outlook.com>
2025-12-03 18:23:34 +0000

    added sqlite

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///elements.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
     return {'hello': 'world'}
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
