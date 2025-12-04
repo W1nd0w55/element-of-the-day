bcf84ea7540a483ba8844667537fcdeb1fa965a8
W1nd0w55 <iliader@outlook.com>
2025-12-04 12:11:51 +0000

    removed prod config (will add it back later when I break up the file into multiple)

--- backend/src/main.py
+++ backend/src/main.py
@@ -9,8 +9,7 @@ db = SQLAlchemy(app)
 def home():
     return {'hello': 'world'}
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=3000, debug=True)
