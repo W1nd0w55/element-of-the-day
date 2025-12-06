1ffdc596a1b15da6f383c3a64d5ef34d69d2d2ec
W1nd0w55 <iliader@outlook.com>
2025-12-06 20:25:49 +0500

    configured docker networks

--- backend/src/main.py
+++ backend/src/main.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
         db.create_all()
         logger.info("Database tables created.")
         logger.warning('a')
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='backend', port=3000, debug=True)
