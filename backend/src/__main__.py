73de6f69ee7ff906d63ca6ad415ccf889b249f7d
W1nd0w55 <iliader@outlook.com>
2025-12-12 21:56:33 +0500

    added waitress

--- backend/src/__main__.py
+++ backend/src/__main__.py
@@ -1,4 +1,5 @@
 from app import app
+from waitress import serve
 from log.loggers import logger
 from sys import argv
 from update_schedule import schedule
@@ -12,8 +13,14 @@ def run_dev() -> None:
     app.run(host='backend', port=3000, debug=True)
 
 
+def run_prod() -> None:
+    serve(app, host='backend', port=3000)
+
+
 if __name__ == '__main__':
     schedule()
     match argv[1]:
         case 'run-dev':
             run_dev()
+        case 'run-prod':
+            run_prod()
