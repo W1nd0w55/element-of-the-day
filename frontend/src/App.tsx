a509e422bce7dc19473e5ded75ba8cf42081762f
W1nd0w55 <iliader@outlook.com>
2025-12-13 19:52:22 +0500

    added tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import type { FC } from 'react';
 
 const App: FC = () => {
-    return <h1>Hi</h1>;
+    return <h1 className='w-[100vw] text-center'>Hi</h1>;
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx';
+import './main.css';
 
 const router = createBrowserRouter([
     { path: '/', element: <App /> }
