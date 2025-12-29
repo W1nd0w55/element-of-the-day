797c2255addfc0963bec057c77430aeeb49a2bb8
W1nd0w55 <iliader@outlook.com>
2025-12-29 15:47:48 +0500

    fixed phyton 3.10

--- backend/src/backend/__init__.py
+++ backend/src/backend/__init__.py
@@ -1,10 +1,10 @@
-from .app import app
+from .app import app as application
 from waitress import serve
 
 
 def run_dev() -> None:
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    application.run(host='0.0.0.0', port=3000, debug=True)
 
 
 def run_prod() -> None:
-    serve(app, host='0.0.0.0', port=3000)
+    serve(application, host='0.0.0.0', port=3000)
