e646b17a70d6434bab4296a5097bc297cdab2739
W1nd0w55 <iliader@outlook.com>
2025-12-31 18:16:28 +0500

    added new year egg

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -2,9 +2,12 @@ import type { DataSpec } from '../common';
 import Element from './Element';
 
 const Content = ({element, colors, red}: DataSpec) => {
+    let today: Date = new Date();
+    let newYear: Date[] = [new Date(today.getFullYear(), 11, 31), new Date(today.getFullYear(), 0, 1)]
+
     return <div className='w-screen h-screen bg-black text-white flex justify-center items-center jbmono'>
         <div className='text-[30pt] flex flex-col text-center items-center'>
-            <div>Today's element is...</div>
+            <div>{today.getDate() === newYear[0].getDate() || today.getDate() === newYear[1].getDate() ? 'Happy New Year!' : 'Today\'s element is...'}</div>
             <Element element={element} colors={colors} red={red} />
         </div>
     </div>;
